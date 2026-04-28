eb2f7dd6064e57d0c728c661c56562d1be7bca0a
Aaron Kaplan <aaron@iri.columbia.edu>
2026-04-28 13:36:51 -0400

    Add launch config

---
+++ catalog/NMME/COLA-RSMAS/CCSM4/index.py
@@ -0,0 +1,31 @@
+import xarray as xr
+
+import pydap_icechunk
+    
+def open(varname) -> xr.Dataset:
+    ds = pydap_icechunk.open_icechunk(f'NMME/COLA-RSMAS/CCSM4/{varname}')
+    ds = ds.rename({
+        'IRIDL_time':  'S',
+        'TIME': 'L',
+        'LAT': 'Y',
+        'LON': 'X',
+        'TIME_expanded': 'target',  # TODO convert target from noleap, or just drop and recreate it
+    })
+    orig_name = next(iter(ds.data_vars))
+    ds = ds.rename({orig_name: varname})
+    # TODO overwrite the attrs wholesale rather than passing through what was saved in the zarr.
+    del ds[varname].attrs['lon']
+    del ds[varname].attrs['lat']
+    ds[varname] = ds[varname].assign_coords(L=('L', range(len(ds['L']))))
+    return ds
+
+vars = {
+    varname: lambda varname=varname: open(varname)
+    for varname in ('prec', 'sst')
+}
+
+if __name__ == '__main__':
+    varname = 'prec'
+    ds = vars[varname]()
+    print(ds)
+    print(ds[varname])
