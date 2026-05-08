14a8ffc086035d1427b536bc05b2e82777489cc0
Rémi COUSIN <remic@iri.columbia.edu>
2026-05-08 12:36:21 -0400

    a first pass at towards an index as dict

--- catalog/NMME/ECCC/CanSIPS-IC4/forecast/index.py
+++ catalog/NMME/ECCC/CanSIPS-IC4/forecast/index.py
@@ -1,40 +1,26 @@
 import xarray as xr
-import numpy as np
-import pandas as pd
 
-import pydap_icechunk
-
+import cataloguing
 
 
 def open(varname) -> xr.Dataset:
-    ds = pydap_icechunk.open_icechunk(
-        f'NMME/ECCC/CanSIPS-IC4/forecast/{varname}'#, decode_times=False
+    return cataloguing.catalogue(
+        varname,
+        varpath='NMME/ECCC/CanSIPS-IC4/forecast',
+        original_names={
+            'start': 'IRIDL_time',
+            'lead': 'step',
+            'latitude': 'latitude',
+            'longitude': 'longitude',
+            'member': 'number',
+            'precipitation': 'prate',
+            'temperature': 'avg_2t',
+            'sea_surface_temperature': 'avg_sst',
+        },
+        drop_variables=["valid_time_expanded", "time"],
+        del_ds_attrs=['history'],
+        lead_is_month=True,
     )
-    # Some varnames have scalar coordinates
-    ds = ds.drop_vars([name for name, coord in ds.coords.items() if coord.dims == ()])
-    ds = ds.drop_vars(["valid_time_expanded", "time"])
-    ds = ds.rename({
-        'IRIDL_time': 'S',
-        'step': 'L',
-        'latitude': 'Y',
-        'longitude': 'X',
-        'number': "M",
-    })
-    original_names = {
-        'prec': 'prate',
-        'tref': 'avg_2t',
-        'sst': 'avg_sst',
-    }
-    if varname in original_names:
-        ds = ds.rename({original_names[varname]: varname})
-    # TODO overwrite the attrs wholesale rather than passing through what was saved in the zarr.
-    del ds.attrs['history'] # temporary until a pydap fix
-    ds = ds.assign_coords(L=('L', range(ds.sizes['L'])))
-    ds = ds.assign_coords(target=pydap_icechunk.S_L_to_target(ds['S'], ds['L']))
-    ds = pydap_icechunk.encode_time(ds)
-    # Force into coords
-    ds[varname].attrs['coordinates'] = "target"
-    return ds
 
 def list_vars():
     return ['prec', 'tref', 'sst']
---
+++ src/cataloguing.py
@@ -0,0 +1,101 @@
+import xarray as xr
+
+import pydap_icechunk
+
+
+COORDS_NAMES = {
+    'start': 'S',
+    'lead': 'L',
+    'latitude': 'Y',
+    'longitude': 'X',
+    'member': 'M',
+    'forecast_time': 'target',
+}
+VARS_NAMES = {
+    'precipitation': 'prec',
+    'temperature': 'tref',
+    'sea_surface_temperature': 'sst',
+}
+ENCODING = {
+    'cf_units': 'hours since 1960-01-01',
+    'calendar': 'standard',
+}
+
+
+def encode_time(
+    ds,
+    cf_catalogue=ENCODING['calendar'],
+    cf_units=ENCODING['cf_units'],
+):
+    time_coords = [
+        coord for coord in ds.coords
+        if ds[coord].dtype in ['datetime64[ns]', 'timedelta64[ns]']
+    ]
+    for tc in time_coords:
+        data, units, calendar = xr.coding.times.encode_cf_datetime(
+            ds[tc], cf_units, cf_catalogue
+        )
+        ds = ds.assign_coords({tc: (ds[tc].dims, data)})
+        ds[tc].attrs['units'] = units
+        ds[tc].attrs['calendar'] = calendar
+    return ds
+
+
+def S_L_to_target(S, L):
+    return xr.DataArray(
+        data=[
+            xr.date_range(
+                start=s.item(),
+                # TODO may not be wise to simply rely on len(L)
+                periods=len(L),
+                freq='MS',
+            )
+            for s in S
+        ],
+        coords=dict(S=S, L=L),
+        attrs={'long_name': 'target date'},
+    )
+
+
+def catalogue(
+    varname,
+    varpath,
+    original_names,
+    drop_variables=None,
+    del_ds_attrs=None,
+    type=None,
+    lead_is_month=False,
+    ):
+    ds = pydap_icechunk.open_icechunk(
+        f'{varpath}/{varname}',
+        drop_variables=drop_variables,
+    )
+    # Some varnames have scalar coordinates that break
+    ds = ds.drop_vars(
+        [name for name, coord in ds.coords.items() if coord.dims == ()]
+    )
+    # Renaming
+    for var in original_names:
+        if var in COORDS_NAMES:
+            ds = ds.rename({original_names[var]: COORDS_NAMES[var]})
+        if var in VARS_NAMES and VARS_NAMES[var] == varname:
+            ds = ds.rename({original_names[var]: varname})
+    # Deleting buggy attributes
+    for attr in del_ds_attrs:
+        del ds.attrs[attr]
+    # Set lead times
+    ds = ds.assign_coords({
+        COORDS_NAMES['lead']: range(ds.sizes[COORDS_NAMES['lead']])
+    })
+    if lead_is_month:
+        # Set target
+        ds = ds.assign_coords({
+            COORDS_NAMES["forecast_time"]: S_L_to_target(
+                ds[COORDS_NAMES['start']], ds[COORDS_NAMES['lead']]
+            )
+        })
+    # Encode time
+    ds = encode_time(ds)
+    # Force into coords
+    ds[varname].attrs['coordinates'] = COORDS_NAMES["forecast_time"]
+    return ds
