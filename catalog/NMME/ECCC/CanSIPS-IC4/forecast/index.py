6164d9749bdbc473f7bdc5e9dde77e9ce387d524
Rémi COUSIN <remic@iri.columbia.edu>
2026-05-12 10:38:29 -0400

    Cansips case fast as well and pint understands kg/m^3

--- catalog/NMME/ECCC/CanSIPS-IC4/forecast/index.py
+++ catalog/NMME/ECCC/CanSIPS-IC4/forecast/index.py
@@ -16,7 +16,7 @@ ORIGINAL_UNITS = {}
 
 def open(varname) -> xr.Dataset:
     ds = pydap_icechunk.open_icechunk(
-        f'NMME/ECCC/CanSIPS-IC4/forecast/{varname}'#, decode_times=False
+        f'NMME/ECCC/CanSIPS-IC4/forecast/{varname}', decode_times=False
     )
     # Some varnames have scalar coordinates
     ds = ds.drop_vars([name for name, coord in ds.coords.items() if coord.dims == ()])
@@ -38,13 +38,13 @@ def open(varname) -> xr.Dataset:
     # TODO overwrite the attrs wholesale rather than passing through what was saved in the zarr.
     del ds.attrs['history'] # temporary until a pydap fix
     ds = ds.assign_coords(L=('L', range(ds.sizes['L'])))
-    ds = ds.assign_coords(target=pydap_icechunk.S_L_to_target(ds['S'], ds['L']))
+    #ds = ds.assign_coords(target=pydap_icechunk.S_L_to_target(ds['S'], ds['L']))
     ureg = UnitRegistry()
     # This is an option but I can't have the program read the file
     #ureg.load_definitions('src/original_untis_def.txt')
     for orig_unit, valid_unit in ORIGINAL_UNITS.items():
         ureg.define(f'{orig_unit} = {valid_unit}')
-    ureg.define('water_density = 1000 * kilogram / m ** 3')
+    ureg.define('water_density = 1000 kg/m^3')
     Q_ = ureg.Quantity
     ds[varname].data = Q_(
         ds[varname].data, ds[varname].attrs['units']
@@ -53,7 +53,7 @@ def open(varname) -> xr.Dataset:
         ds[varname].data = ds[varname].data / ureg.water_density
     ds[varname].data = ds[varname].data.to(VARS_UNITS[varname])
     ds[varname].attrs['units'] = VARS_UNITS[varname]
-    ds = pydap_icechunk.encode_time(ds)
+    #ds = pydap_icechunk.encode_time(ds)
     # Force into coords
     ds[varname].attrs['coordinates'] = "target"
     return ds
