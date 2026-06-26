5800e6d4c2d296ecf16ee39c6439b9334e06a9c5
Aaron Kaplan <aaron@iri.columbia.edu>
2026-06-26 15:13:14 -0400

    On failure, show all discrepancies instead of just the first one

--- ingestion/test/test_regression.py
+++ ingestion/test/test_regression.py
@@ -1,6 +1,6 @@
 from collections.abc import Mapping
 import itertools
-from typing import Any
+from typing import Any, cast
 
 import icechunk
 import numpy as np
@@ -60,24 +60,38 @@ def test_it(var_path: str):
     )
     assert new_count
     session.commit(f'update from {descriptor.dir}')
-    ds_new = xr.open_zarr(session.store)
-    ds_old = (
+    ds_new = remove_irrelevant(xr.open_zarr(session.store))
+    ds_old = remove_irrelevant(
         preprocess.open_icechunk(icechunk_info.relpath)
         .isel(IRIDL_time=slice(0,2))
     )
-    xarray.testing.assert_equal(ds_old, ds_new)
-    assert ds_old.attrs == ds_new.attrs
-    for var in itertools.chain(ds_old.coords, ds_old.data_vars):
-        assert ds_old[var].attrs == ds_new[var].attrs
-        assert_encodings_equal(ds_old[var].encoding, ds_new[var].encoding)
+    xarray.testing.assert_identical(ds_new, ds_old)
+    encodings_old = {name: replace_nans(v.encoding) for name, v in variables_of(ds_old).items()}
+    encodings_new = {name: replace_nans(v.encoding) for name, v in variables_of(ds_new).items()}
+    assert encodings_new == encodings_old
 
-def assert_encodings_equal(d1: Mapping[str, Any], d2: Mapping[str, Any]):
-    assert set(d1.keys()) == set(d2.keys())
-    for k in d1:
-        v1 = d1[k]
-        v2 = d2[k]
-        # nan == nan is False, so we need a special case for it.
-        if isinstance(v1, float) and np.isnan(v1):
-            assert np.isnan(v2), f'different values for {k}: {v1} != {v2}'
-        else:
-            assert v1 == v2, f'different values for {k}: {v1} != {v2}'
+class NaNSentinel:
+    def __repr__(self):
+        return "NaN"
+NAN = NaNSentinel()
+
+def replace_nans(d: dict[str, Any]) -> dict[str, Any]:
+    return {k: replace_nan(v) for k, v in d.items()}
+
+def replace_nan(v):
+    if isinstance(v, float) and np.isnan(v):
+        return NAN
+    return v
+
+def variables_of(ds: xr.Dataset):
+    assert all(isinstance(k, str) for k in ds.variables)
+    return cast(Mapping[str, xr.Variable], ds.variables)
+
+def remove_irrelevant(ds: xr.Dataset):
+    # cfgrib creates 'history' and 'source' attributes that change on every run.
+    # The former contains a timestamp, and the latter depends on $CWD.
+    attrs = ds.attrs.copy()
+    for attr in ('history', 'source'):
+        attrs.pop(attr, None)
+
+    return xr.Dataset(ds.data_vars, ds.coords, attrs)
