6e4af0ddd6dd30b7bb5976291185dd0b46ef05bf
Andy Robertson <awr2001@columbia.edu>
2026-07-30 18:02:28 -0400

    modified harmonize.py

--- server/src/sigrid/harmonize.py
+++ server/src/sigrid/harmonize.py
@@ -109,6 +109,8 @@ STANDARD_UNIT_CONVERSIONS: Mapping[tuple[str, str], UnitConverter] = MappingProx
     ('m/s', 'mm/day'): linear_converter(0, 1000 * 60 * 60 * 24),
     ('m s**-1', 'mm/day'): linear_converter(0, 1000 * 60 * 60 * 24),
     ('m/s', 'm s-1'): null_converter,
+    ('m/s', 'm s**-1'): null_converter,
+    ('m s**-1', 'm/s'): null_converter,
     ('hPa', 'Pa'): linear_converter(0, 100),
     # Volumetric latent heat of vaporization: 2453 MJ m-3
     ('watt/m^2', 'mm/day'): linear_converter(0, 1000 * 60 * 60 * 24 / 2453e6),
